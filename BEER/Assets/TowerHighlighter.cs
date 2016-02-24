30aa90f51db10602252c7a45deee2c2b44ab975b
Patrick Seebacher <patrick.seebacher@student.tugraz.at>
2016-02-24 15:03:33 +0100

    TowerHighlighter script

---
+++ BEER/Assets/TowerHighlighter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerHighlighter : MonoBehaviour {
+
+  private Color startcolor;
+  private MeshRenderer MyRenderer;
+
+  // Use this for initialization
+  void Start () {
+    MyRenderer = GetComponentInChildren<MeshRenderer>();
+  }
+
+  
+  void OnMouseEnter()
+  {
+    startcolor = MyRenderer.material.color;
+    MyRenderer.material.color = Color.yellow;
+  }
+  void OnMouseExit()
+  {
+    MyRenderer.material.color = startcolor;
+  }
+}
